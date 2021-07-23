a4a743bc3440a36c9873a8c8967f85afcb51883a
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-23 10:42:30 -0700

    create restricted middleware

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,7 +1,18 @@
 const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../secrets/index");
 
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization;
+  if (!token) {
+    return next({ status: 401, message: "token required" });
+  }
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    if (err) {
+      return next({ status: 401, message: "token invalid" });
+    }
+    req.decodedToken = decodedToken;
+    next();
+  });
   /*
     IMPLEMENT
 
