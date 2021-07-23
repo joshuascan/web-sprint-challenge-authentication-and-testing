9d8c44b28a33125edf53f33e8765b53ea095d15b
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-23 10:07:13 -0700

    create middleware for register endpoint

--- api/middleware/auth-middleware.js
+++ api/middleware/auth-middleware.js
@@ -1,3 +1,27 @@
 const db = require("../../data/dbConfig");
 
-const validateNewUser = async (req, res, next) => {};
+const checkUsernameAvailable = async (req, res, next) => {
+  try {
+    const existing = await db("users")
+      .where("username", req.body.username)
+      .first();
+    if (existing) {
+      next({ status: 422, message: "username taken" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+const validateNewUser = (req, res, next) => {
+  const { username, password } = req.body;
+  if (username === undefined || password === undefined) {
+    next({ status: 422, message: "username and password required" });
+  } else {
+    next();
+  }
+};
+
+module.exports = { checkUsernameAvailable, validateNewUser };
