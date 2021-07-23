cadbc1a6c0004d521e803bb687c1f6b35fd5b961
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-23 12:06:09 -0700

    builds tests for get jokes endpoint

--- api/server.test.js
+++ api/server.test.js
@@ -57,13 +57,56 @@ describe("[POST] /api/auth/login", () => {
     });
     expect(res.status).toBe(200);
   });
-  test("responds with correct message and status when using invalid credentials", async () => {
+  test("responds with correct error message and status when using invalid credentials", async () => {
     const err = await request(server)
       .post("/api/auth/login")
       .send({ username: "I don't exist", password: "wrong" });
     expect(err.body.message).toBe("invalid credentials");
     expect(err.status).toBe(401);
   });
+  test("responds with correct error message and status code when missing username/password", async () => {
+    const err = await request(server).post("/api/auth/login");
+    expect(err.body.message).toBe("username and password required");
+    expect(err.status).toBe(422);
+  });
 });
 
-describe;
+describe("[GET] /api/jokes", () => {
+  test("responds with an array of jokes when accessed with correct token", async () => {
+    await request(server)
+      .post("/api/auth/register")
+      .send({ username: "John", password: "Shmoe" });
+    const res = await request(server)
+      .post("/api/auth/login")
+      .send({ username: "John", password: "Shmoe" });
+    const jokes = await request(server)
+      .get("/api/jokes")
+      .set("authorization", res.body.token);
+    expect(jokes.body).toMatchObject([
+      {
+        id: "0189hNRf2g",
+        joke: "I'm tired of following my dreams. I'm just going to ask them where they are going and meet up with them later.",
+      },
+      {
+        id: "08EQZ8EQukb",
+        joke: "Did you hear about the guy whose whole left side was cut off? He's all right now.",
+      },
+      {
+        id: "08xHQCdx5Ed",
+        joke: "Why didn’t the skeleton cross the road? Because he had no guts.",
+      },
+    ]);
+  });
+  test("responds with correct error message and status when accessed without token", async () => {
+    const err = await request(server).get("/api/jokes");
+    expect(err.body.message).toBe("token required");
+    expect(err.status).toBe(401);
+  });
+  test("responds with correct error message and status when accessed using invalid token", async () => {
+    const err = await request(server)
+      .get("/api/jokes")
+      .set("authorization", "abcd1234");
+    expect(err.body.message).toBe("token invalid");
+    expect(err.status).toBe(401);
+  });
+});
